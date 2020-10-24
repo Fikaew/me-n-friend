43b611f8839216a38cac4fda953a33cac80471fe
FIKAEW CHAYAWATTANA <63010719@KMITL.AC.TH>
2020-10-24 23:27:56 +0700

    -add pyramid

--- me 'n friend/Source.cpp
+++ me 'n friend/Source.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 int* max(int* a, int* b);
 int main()
 {
@@ -8,12 +9,25 @@ int main()
 	{
 		scanf_s("%d", &num[i]);
 	}
-	printf("\nMaximum Value : %d", *max(max(num, num++), max(num++, num++)));
+	printf("\nMaximum Value : %d\n", *max(max(num, num++), max(num++, num++)));
+	int x = *max(max(num--, num--), max(num--, num));
+	printf("%d\n", x);
+	for (int j = 1; j <= abs(x); j++)
+		{
+			for (int k = 1; k <= j; k++)
+			{
+				printf("* ");
+				for (int l = j; l <= k; l++)
+				{
+					printf("\n");
+				}
+			}
+		}
 	return 0;
 }
 int* max(int* a, int* b)
 {
-	if (*a >= *b)
+	if (*a > *b)
 	{
 		return a;
 	}
